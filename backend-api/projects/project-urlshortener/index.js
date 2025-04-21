844c8b73172751e769ae3d839a16a270051962b7
lowercase-tousif <toisiftasrik@gmail.com>
2025-04-21 23:43:11 +0600

    Modified a little bit | used bodyParse middleware and added a comment

--- backend-api/projects/project-urlshortener/index.js
+++ backend-api/projects/project-urlshortener/index.js
@@ -1,24 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-
+const bodyParser = require("body-parser");
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+// Edited by tousif tasrik (21.04.2025)
+
+// Dnslookup
+
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
